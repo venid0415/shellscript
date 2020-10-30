d4e2f53e27a85087232e1056263cb31253e4d21f
venid0415 <venid0415@gamil.com>
2020-10-30 20:06:42 +0000

    script 7

---
+++ scp7.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ADDF(){
+      echo "addition = $(($a+$b))"
+      }
+  SUBF()
+  
+    {
+         echo "subtract = $(($a-$b))"
+    }
+##main program
+  
+  a=10
+  b=5
+  ADDF
+  SUBF
+
+
